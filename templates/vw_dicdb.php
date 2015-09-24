6dd8f981a5327a9ab48040a31caa219e3b39e716
Jorge Alberto Ponce Turrubiates <the.yorch@gmail.com>
2015-09-24 13:11:18 -0500

    Remote FontAwesome

--- templates/vw_dicdb.php
+++ templates/vw_dicdb.php
@@ -230,7 +230,7 @@
     <script src="./metro-bootstrap/bootstrap.min.js"></script>
     <script src="./metro-bootstrap/bootstrap-tooltip.js"></script>
     <script src="./js/bootbox.js"></script>
-    <link rel="stylesheet" href="./metro-bootstrap/font-awesome.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     <script>
       $(document).ready(function(){
